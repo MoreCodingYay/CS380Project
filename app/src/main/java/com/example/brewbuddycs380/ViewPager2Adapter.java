33f04b8a5f1c4523bd66c815c25052b5c4adaadb
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2023-05-16 20:41:32 -0700

    java code to navigate between fragments for questions

---
+++ app/src/main/java/com/example/brewbuddycs380/ViewPager2Adapter.java
@@ -0,0 +1,31 @@
+package com.example.brewbuddycs380;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import java.util.ArrayList;
+
+public class ViewPager2Adapter extends FragmentStateAdapter {
+
+    // fragments that will be swiped between
+    private ArrayList<Fragment> fragments;
+
+    // constructor
+    public ViewPager2Adapter(FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+
+    }
+
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    public int getItemCount() {
+        return fragments.size();
+    }
+
+    public void setData(ArrayList<Fragment> fragments) {
+        this.fragments = fragments;
+    }
+}
